1a9eb4eab3b75c2f93d4420835de78ac38a95b8c
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-06-03 23:23:57 +0330

    coded a library in solidity

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,35 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
-    uint256 public minUSD = 1;
+    using PriceConverter for uint256;
 
-    AggregatorV3Interface internal priceFeed;
+    uint256 public minUSD = 1;
 
     address[] public funders;
 
     mapping(address => uint256) public addressToMoney;
 
-    constructor(address _priceFeed) {
-        priceFeed = AggregatorV3Interface(_priceFeed);
-    }
-
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minUSD, "Not enough funds!");
+        require(msg.value.getConversionRate() >= minUSD, "Not enough funds!");
         funders.push(msg.sender);
         addressToMoney[msg.sender] = msg.value;
-    }
-
-    function getPrice() public view returns (uint256) {
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        return ((getPrice() * ethAmount) / 1e18);
     }
 }
