6ae566538e0bfd6d9c966b183949c3d242c074df
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-06-03 22:50:01 +0330

    FundMe.sol file created and coded

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,17 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-// Get Funds from users
-// Withdraw funds
-// Set a minimum funding value in USD
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 etherToWei = 1e18;
-    uint256 etherToGWei = 1e9;
+    uint256 public minUSD = 1;
 
-    function fund() public payable {
-        require(msg.value > etherToWei, "Not enough funds!");
+    AggregatorV3Interface internal priceFeed;
+
+    address[] public funders;
+
+    mapping(address => uint256) public addressToMoney;
+
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
-    function withdraw() internal {}
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minUSD, "Not enough funds!");
+        funders.push(msg.sender);
+        addressToMoney[msg.sender] = msg.value;
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        return ((getPrice() * ethAmount) / 1e18);
+    }
 }
