f5b80bcc822fdbea4bf6157a8efcf0a03a08d8bc
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-06-04 01:23:13 +0330

    Added withdraw functionality and 3 ways to do it

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -17,4 +17,39 @@ contract FundMe {
         funders.push(msg.sender);
         addressToMoney[msg.sender] = msg.value;
     }
+
+    function withdraw() public {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToMoney[funder] = 0;
+        }
+
+        // reset the arrasy of funders
+        funders = new address[](0);
+
+        // withdraw the funds
+        /**
+         * 1. Transfer
+         * 2. Send
+         * 3. Call
+         */
+
+        // transfer - if anything goes wrong, thr transaction is reverted
+        payable(msg.sender).transfer(address(this).balance);
+
+        // send - if anything goes wrong the contract returns boolean
+        bool result = payable(msg.sender).send(address(this).balance);
+        require(result, "Send failed!");
+
+        // call - it's way more advanced level of creating a transaction
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success, "Call failed!");
+    }
+    // Also check: https://solidity-by-example.org/sending-ether
 }
